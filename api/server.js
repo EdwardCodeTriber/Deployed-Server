2f739915bf047f531fd6e1db283d2fd245a792ba
EdwardThapelo <edwardthapelo55@gmail.com>
2024-10-11 11:57:27 +0200

    New directory

--- api/server.js
+++ api/server.js
@@ -3,9 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World On server');
+  res.send('Hello World On server \n Express on Vercel');
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
